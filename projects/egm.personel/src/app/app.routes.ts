ee7396dd47b967e5c4f0be30737434d3cf9da7b7
mcavdar89 <mcavdar89@gmail.com>
2024-12-16 14:17:37 +0300

    form

--- projects/egm.personel/src/app/app.routes.ts
+++ projects/egm.personel/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'personel/form', loadComponent: () => import('./pages/personel-form/personel-form.component').then(m => m.PersonelFormComponent) },
+];
--- projects/egm.personel/src/app/pages/personel-form/personel-form.component.ts
+++ projects/egm.personel/src/app/pages/personel-form/personel-form.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+
+
 
 @Component({
   selector: 'app-personel-form',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule, InputTextModule,DropdownModule ],
   templateUrl: './personel-form.component.html',
   styleUrl: './personel-form.component.scss'
 })
-export class PersonelFormComponent {
+export class PersonelFormComponent implements OnInit {
+
+  frmPersonel: FormGroup;
+
+  unvanList =[
+    {ad:'Yönetici', id:1},
+    {ad:'Müdür', id:2},
+    {ad:'Uzman', id:3},
+    {ad:'Personel', id:4}
+
+  ]
+
+  constructor(private fb:FormBuilder) {
+    this.frmPersonel = this.fb.group(
+      {
+        id:'',
+        ad: ['', [Validators.required]],
+        soyad: ['', [Validators.required]],
+        ePosta: ['', [Validators.required, Validators.email]],
+        unvanId: ['', [Validators.required]],
+        dogumTarihi: ['', [Validators.required]],
+      }
+    );
+   }
+
+  ngOnInit(): void {
+  }
+
+
 
 }
