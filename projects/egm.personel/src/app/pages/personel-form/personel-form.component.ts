f2bd6252c2e8c1c3627ded7dfd14dde1bfb21b7c
mcavdar89 <mcavdar89@gmail.com>
2024-12-16 13:43:31 +0300

    personel component

---
+++ projects/egm.personel/src/app/pages/personel-form/personel-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personel-form',
+  imports: [],
+  templateUrl: './personel-form.component.html',
+  styleUrl: './personel-form.component.scss'
+})
+export class PersonelFormComponent {
+
+}
