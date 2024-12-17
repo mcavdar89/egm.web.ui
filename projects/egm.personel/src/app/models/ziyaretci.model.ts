330306aa1828619face79306c50f3fcb25b30beb
mcavdar89 <mcavdar89@gmail.com>
2024-12-17 13:52:51 +0300

    .

---
+++ projects/egm.personel/src/app/models/ziyaretci.model.ts
@@ -0,0 +1,13 @@
+export interface Ziyaretci {
+    id:number;
+    ad:string;
+    soyad:string;
+    tcKimlikNo:string;
+    telefon:string;
+    email:string;
+    ziyaretTarihi:string;
+    misafirVarMi:boolean;
+    misafirAd:string;
+    misafirSoyad:string;
+    misafirTcKimlikNo:string;
+}
---
+++ projects/egm.personel/src/app/pages/ziyaretci-kayit-form/ziyaretci-kayit-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ziyaretci-kayit-form',
+  imports: [],
+  templateUrl: './ziyaretci-kayit-form.component.html',
+  styleUrl: './ziyaretci-kayit-form.component.scss'
+})
+export class ZiyaretciKayitFormComponent {
+
+}
